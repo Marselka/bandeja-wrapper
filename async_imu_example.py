b8a5f3ae26afeb5b82e4d20e5ad690c0c3e8a11d
Marsel Faizullin <ma.faizullin@gmail.com>
2021-03-01 07:38:37 +0300

    half way to launch all the scripts and programs by single .py script

--- async_imu_example.py
+++ async_imu_example.py
@@ -3,33 +3,80 @@ from src.RemoteControl import RemoteControl
 from concurrent.futures import ThreadPoolExecutor
 import subprocess
 import rospy
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import Imu, CameraInfo, TimeReference
 import numpy as np
 import pandas as pd
 from io import StringIO
 from src.TimeSync import TimeSync2
 
 import matplotlib.pyplot as plt
+import signal
+import sys
 
-HOST = '10.30.65.167'  # The smartphone's IP address
+HOST = '10.30.65.137'  # The smartphone's IP address
 
 mcu_imu_time = []
 mcu_imu_data = []
+global stop_flag
+stop_flag = 0
 
-def callback(data):
+#global depth_cam_ts
+depth_cam_ts = []#{'secs': None, 'nsecs' : None}
+
+#global mcu_cam_ts
+mcu_cam_ts = []#{'secs': None, 'nsecs' : None}
+
+def mcu_imu_callback(data):
     dat = data.header.stamp.secs + data.header.stamp.nsecs  / 1e9
     mcu_imu_time.append(dat)
 
     dat = data.angular_velocity
     mcu_imu_data.append([dat.x, dat.y, dat.z])
 
-def listener():
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("mcu_imu", Imu, callback)
-    #rospy.spin()
+def depth_cam_callback(data):
+    if data.header.seq == 1:
+        depth_cam_ts.append(data.header.stamp)
+        print data.header.seq, data.header.frame_id
 
+def mcu_cam_callback(data):
+    if data.header.seq == 12:
+        mcu_cam_ts.append(data.header.stamp)
+        print data.header.seq, data.header.frame_id
+        #global mcu_cam_ts
+        #mcu_cam_ts['secs'] = data.header.stamp.secs
+        #mcu_cam_ts['nsecs'] = data.header.stamp.nsecs
+
+        
 def main():
+
+
+    '''msg = TimeReference()
+    msg.header.frame_id = "mcu_depth_ts"
+    msg.header.stamp.secs = 1
+    msg.header.stamp.nsecs = 1
+    msg.time_ref.secs = 1
+    msg.time_ref.nsecs = 1
+
+    msg2 = TimeReference()
+    msg2.header.stamp = msg.header.stamp
+    print msg2#.header.stamp
+
+    exit()'''
+    def signal_handler(sig, frame):
+        print('You pressed Ctrl+C. Exiting.')
+        global stop_flag
+        stop_flag = 1
+
+    signal.signal(signal.SIGINT, signal_handler)
+
     remote = RemoteControl(HOST)
+
+    launch_process = subprocess.Popen("roslaunch data_collection data_collection_ns.launch".split())
+    time.sleep(3)
+
+    rospy.init_node('listener', anonymous=True)
+
+# 1. Twist-n-Sync 
     start_duration = 1
     main_duration = 4
     end_duration = 5
@@ -37,13 +84,19 @@ def main():
     with ThreadPoolExecutor(max_workers=1) as executor:
         print('imus gathering started')
         future = executor.submit(remote.get_imu, 1000 * (start_duration + main_duration + end_duration), False, True)
-        listener()
+        #mcu_imu_listener()
+
+        mcu_imu_listener = rospy.Subscriber("mcu_imu", Imu, mcu_imu_callback)
+
         time.sleep(start_duration)
         print('start shaking')
         time.sleep(main_duration)
         print('put back')
         time.sleep(end_duration)
-        rospy.signal_shutdown('it is enough')
+
+        #rospy.signal_shutdown('it is enough')
+        mcu_imu_listener.unregister()
+
         accel_data, gyro_data = future.result()
         print('stopped')
 
@@ -57,8 +110,6 @@ def main():
     sm_gyro_data = sm_df.iloc[1:, :3].to_numpy()
     sm_gyro_time = sm_df.iloc[1:, 3].to_numpy() / 1e9
     
-    #print(mcu_gyro_data.shape, sm_gyro_data.shape, mcu_gyro_time.shape, sm_gyro_time.shape)
-
     # Equalize lengths
     min_length = min(sm_gyro_time.shape[0], mcu_gyro_time.shape[0])
     mcu_gyro_data, mcu_gyro_time, sm_gyro_data, sm_gyro_time = \
@@ -73,23 +124,46 @@ def main():
     time_sync2.obtain_delay()
     comp_delay2 = time_sync2.time_delay
 
+    # Compute resulting offset
+    sm_mcu_clock_offset = np.mean(sm_gyro_time - mcu_gyro_time) + comp_delay2 #sm_mcu_clock_offset = (sm_gyro_time[0] - mcu_gyro_time[0] + comp_delay2)
+
+    # Show mean of omegas to visually oversee sync performance
+    plt.ion()
+    plt.plot(mcu_gyro_time, np.mean(mcu_gyro_data, axis=1))
+    plt.plot(sm_gyro_time - sm_mcu_clock_offset, np.mean(sm_gyro_data, axis=1), '--')
+    plt.show()
+    plt.pause(2)
+    plt.close()
+# 2. Azure camera alignment
+    depth_cam_listener = rospy.Subscriber("/azure/depth/camera_info", CameraInfo, depth_cam_callback)
+    mcu_cam_listener = rospy.Subscriber("/mcu_cameras_ts", TimeReference, mcu_cam_callback)
+
     # Send start_mcu_cam_triggering command to mcu via mcu.cpp
-    bashCommand = "rosrun mcu_interface start_mcu_cam_trigger_client"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+    cam_align_process = subprocess.Popen("rosrun mcu_interface start_mcu_cam_trigger_client".split()) 
 
     # Some time needed to get a camrera frame and its info in mcu.cpp
     time.sleep(0.1)
+    
+    pub = rospy.Publisher('wewerqwerqewrqwerddsdasd', TimeReference, latch=True, queue_size=10)
+    
+    while len(mcu_cam_ts) != 1 or len(depth_cam_ts) != 1:
+        time.sleep(0.01)
+    depth_cam_listener.unregister()
+    mcu_cam_listener.unregister()
+    msg = TimeReference()
+    msg.header.frame_id = "mcu_depth_ts"
+    msg.header.stamp = mcu_cam_ts[0]
+    msg.time_ref = depth_cam_ts[0]
+    pub.publish(msg)
 
-    # Compute resulting offset
-    sm_mcu_clock_offset = np.mean(sm_gyro_time - mcu_gyro_time) + comp_delay2    #sm_mcu_clock_offset = (sm_gyro_time[0] - mcu_gyro_time[0] + comp_delay2)
-
-    # Start the video in s10
+    # Start video on s10
     sm_remote_ts_ns, sm_frame_period_ns = remote.start_video()
-    sm_remote_ts = sm_remote_ts_ns / 1e9; sm_frame_period = sm_frame_period_ns / 1e9
+    sm_remote_ts = sm_remote_ts_ns / 1e9;
+    #sm_frame_period = sm_frame_period_ns / 1e9
 
+    # Compute mcu desired timestamp
     mcu_desired_ts = sm_remote_ts - sm_mcu_clock_offset
-
+    '''
     # Save some info 
     print "comp_delay2        ", comp_delay2
     print "sm_mcu_clock_offset", sm_mcu_clock_offset
@@ -106,47 +180,27 @@ def main():
                 str(comp_delay2) + ',' + str(sm_remote_ts) + ',' + str(mcu_desired_ts) + ',' + str(sm_mcu_clock_offset) + \
                 '\n'
     )
-    #print(sm_mcu_clock_offset, sm_gyro_time[0] - mcu_gyro_time[0], mcu_gyro_data.shape, sm_gyro_data.shape, mcu_gyro_time.shape, sm_gyro_time.shape)
-
-    # Video duration before phase alignment
-    time.sleep(2)
+    '''
 
     # Phase alignment
-    bashCommand = "rosrun mcu_interface align_mcu_cam_phase_client " + str(mcu_desired_ts)
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+    align_camera_process = subprocess.Popen(("rosrun mcu_interface align_mcu_cam_phase_client " + str(mcu_desired_ts)).split())
 
-    # TODO subscribe/unsubscribe
-    #mcu_imu_time[:] = []
-    #mcu_imu_data[:] = []
-    #flag_is_read_once = False
-    #flag_to_read = True
-    #while flag_is_read_once == False:
-    #    time.sleep(1e-6)
-    #rospy.signal_shutdown('it is enough')
-    
-    # Some time needed to get a camrera frame
+    # Some time needed to get camrera frame data by mcu.cpp
     time.sleep(0.1)
-    #mcu_timestamp = mcu_imu_time[0]
-    #s10_timestamp = mcu_timestamp + sm_mcu_clock_offset
-    
-    # Send publish_s10_timestamp message to mcu.cpp
-    # sm_mcu_clock_offset = s10_timestamp - mcu_timestamp
-    bashCommand = "rosrun mcu_interface publish_s10_to_mcu_offset_client " + str(sm_mcu_clock_offset)
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
 
-    # Video duration before phase alignment    
-    time.sleep(2)
+    # Send publish_s10_timestamp message to mcu.cpp
+    send_offset_subprocess = subprocess.Popen(("rosrun mcu_interface publish_s10_to_mcu_offset_client " + str(sm_mcu_clock_offset)).split())
+    
+    ### Add record 
+
+
+    print('Recording started.\nPress Ctrl+C to stop and exit')
+
+    while stop_flag == 0:
+        time.sleep(0.01);
 
     remote.stop_video()
     remote.close()
-    
-    # Show mean of omegas to visually oversee sync performance
-    plt.plot(mcu_gyro_time, np.mean(mcu_gyro_data, axis=1))
-    plt.plot(sm_gyro_time - sm_mcu_clock_offset, np.mean(sm_gyro_data, axis=1), '--')
-
-    plt.show()
-
+    depth_cam_listener.unregister()
 if __name__ == '__main__':
     main()
