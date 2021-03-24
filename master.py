c01ae756f8b4121c0c86b5129928d2e1aebbb4b0
Marsel Faizullin <ma.faizullin@gmail.com>
2021-03-24 18:39:43 +0300

    add interaction with timestamped sequences messages

--- master.py
+++ master.py
@@ -90,7 +90,10 @@ def main(args):
                 running_subpr_list.append(subpr)
         exit_codes = [p.wait() for p in running_subpr_list]
         if remote is not None:
-            remote.stop_video()
+            try:
+                remote.stop_video()
+            except:
+                pass
             remote.close()
         sys.exit()
         #global stop_flag
@@ -252,35 +255,34 @@ def main(args):
     send_offset_subprocess = subprocess.Popen(("rosrun mcu_interface publish_s10_to_mcu_offset_client " + str(sm_mcu_clock_offset)).split())#subpr_list.append(send_offset_subprocess)
     send_offset_subprocess.wait()
 # 3. Record data
-    #record_subprocess = subprocess.Popen(('rosrun data_collection record_all.sh').split())
-    #subpr_list.append(record_subprocess)    
+    record_subprocess = subprocess.Popen(('rosrun data_collection record_all.sh').split())
+    subpr_list.append(record_subprocess)    
 
     time.sleep(1)
     
     print_master('Recording is started')#\nPress Ctrl+C to stop recording along with everything and exit')
 
-    publisher_indicator = rospy.Publisher('/indicator_to_process_data', TimeReference, latch=True, queue_size=10)
+    publisher_indicator = rospy.Publisher('/sequences_ts', TimeReference, latch=True, queue_size=10)
 
     #flag_to_process = True
-    sequence = 1
-    print_master('Current sequence: ' + str(sequence))
+    sequence_num = 1
+    print_master('Current sequence number: ' + str(sequence_num))
     print_master('Tap Enter to indicate the next sequence')
 
-    while True:#stop_flag == 0:
-        '''input = select.select([sys.stdin], [], [], 0.01)[0]
+    while True:
+        input = select.select([sys.stdin], [], [], 0.01)[0]
         if input:
             value = sys.stdin.readline().rstrip() 
             if (value == ""):
-                #flag_to_process = not flag_to_process
                 msg = TimeReference()
                 #msg.header.frame_id = "mcu_depth_ts"
                 msg.header.stamp = mcu_cam_ts_common
                 #msg.time_ref = depth_cam_ts
-                msg.source = str(sequence)
+                msg.source = str(sequence_num)
                 publisher_indicator.publish(msg)
-        
-                print_master('Current sequence:', sequence)
-                print_master('Tap Enter to indicate the next sequence')'''
+                sequence_num += 1
+                print_master('Current sequence: ' + str(sequence_num))
+                print_master('Tap Enter to indicate the next sequence')
         time.sleep(0.01);
 
     #remote.stop_video()
