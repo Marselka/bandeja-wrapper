c4b924e621e235d216b937e0f75a49d513613651
Marsel Faizullin <ma.faizullin@gmail.com>
2021-03-24 14:49:55 +0300

    add Singular matrix handling in TimeSync class

--- src/TimeSync.py
+++ src/TimeSync.py
@@ -36,6 +36,7 @@ class TimeSync2:
         self.xx2_new = None
         self.time_delay = None
         self.cor = None
+        self.calibration_is_succeded = None
         
     def resample(self, accuracy=1e-3):
         # if... else... can be skipped if data has the same constant data rate
@@ -97,9 +98,15 @@ class TimeSync2:
         #print(xx1_temp.shape, xx2_temp.shape)
         
         # Calibration
-        self.M = np.matmul( np.matmul(xx2_temp.T, xx1_temp), np.linalg.inv(np.matmul(xx1_temp.T, xx1_temp)))
-        self.xx1_new = np.matmul(self.M, self.xx1_new.T).T
-        
+        try:
+            pseudoinverse = np.linalg.inv(np.matmul(xx1_temp.T, xx1_temp))
+            self.M = np.matmul( np.matmul(xx2_temp.T, xx1_temp), pseudoinverse)
+            self.xx1_new = np.matmul(self.M, self.xx1_new.T).T
+            self.calibration_is_succeded = True
+        except:
+            print('Cannot calibrate. Pseudoinverse is not defined\nKeeping data the same')
+            self.calibration_is_succeded = False
+
         # Cross-cor. reestimation
         cross_cor, index_init = get_initial_index(self)
 
